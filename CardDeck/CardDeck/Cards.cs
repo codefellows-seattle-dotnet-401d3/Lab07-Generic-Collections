625c8c2ae1051e48fbbdd2b4452398684fc595cd
Playingtygre <kymiehsu123@gmail.com>
2018-03-28 19:39:15 -0700

    added some stuff

---
+++ CardDeck/CardDeck/Cards.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardDeck
+{
+    class Cards
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //EnumExample();
+            DeckExample();
+
+       
+        }
+
+
+        static void DeckExample()
+        {
+            List<string> myList = new List<string>();
+            myList.Add("Ace of Hearts");
+            myList.Add("Two of Hearts");
+            myList.Add("Three of Hearts");
+            myList.Add("Four of Hearts");
+            myList.Add("Five of Hearts");
+            myList.Add("Six of Hearts");
+
+
+        }
+
+    }
+}
--- CardDeck/CardDeck/Program.cs
+++ CardDeck/CardDeck/Program.cs
@@ -6,7 +6,25 @@ namespace CardDeck
     {
         static void Main(string[] args)
         {
+
+            //Look at you tube videos from 3/27/18 end of parts 1 0f 3
+
             Console.WriteLine("Hello World!");
+
+            enumExample();
+            Console.ReadLine();
+
+
+        }
+
+
+        // Method that calls Hearts, which is stated in CardDeck.cs.
+        // remeber that deal is a local variable which calls 
+        static void enumExample()
+        {
+           // Hearts deal = Hearts.Jack;
+            int daynumber = (int)deal;
+            Console.WriteLine(daynumber);
         }
     }
 }
